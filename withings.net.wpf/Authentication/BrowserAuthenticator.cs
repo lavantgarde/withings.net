75c87d36d61b1c0cc8aa23e45563aedf86bf2c49
lavantgarde <32887314+lavantgarde@users.noreply.github.com>
2020-01-17 20:53:00 -0800

    add withings wpf sdk

---
+++ withings.net.wpf/Authentication/BrowserAuthenticator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using withings.net.Authentication;
+using withings.net.Client;
+
+namespace withings.net.wpf.Authentication
+{
+    class BrowserAuthenticator : IAuthentication
+    {
+        public Task<string> GetGetAuthorizationCode()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
